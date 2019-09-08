63d04522f97b71c4383562054ca55df66fcdb23c
Benoît Sacleux <benoitsacleux@outlook.fr>
2019-09-08 12:28:51 +0200

    add function init,refreshcanvas in javascript.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 window.onload = function(){
-    let canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    document.body.appendChild(canvas);
 
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle= "#ff0000";
-    ctx.fillRect(30 ,30 ,100 ,50);
+    let canvas;
+    let ctx;
+    let delay = 1000;
+    let xCoord = 0;
+    let yCoord = 0;
+
+    init();
+
+    function init(){
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+        
+    }
+    
+    function refreshCanvas(){
+        xCoord += 2;
+        yCoord += 2;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(xCoord ,yCoord ,100 ,50);
+    }
 }
